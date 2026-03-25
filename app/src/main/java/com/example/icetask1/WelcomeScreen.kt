2a9167ff1ec2c676432e61f50f74a2bfa5e4c89f
ThorisoT <st10191543@myemeris.edu.za>
2026-03-25 20:21:52 +0200

    Final Ice Task

--- app/src/main/java/com/example/icetask1/WelcomeScreen.kt
+++ app/src/main/java/com/example/icetask1/WelcomeScreen.kt
@@ -1,20 +1,22 @@
 package com.example.icetask1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class WelcomeScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_welcome_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
